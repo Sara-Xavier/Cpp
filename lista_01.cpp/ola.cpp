97513cb8b85311e90e9a6cbb962da0d0410f12ac
Sara_Xavier <saraaxavierrr@gmail.com>
2024-03-05 21:52:41 -0300

    A questão (a) da 01 precisa ser testada em compilador C/Cpp.

---
+++ lista_01.cpp/ola.cpp
@@ -0,0 +1,18 @@
+// A partir do programa ola.cpp a seguir
+
+// Listing 1: ola.cpp
+
+// #include <iostream>
+// int main(){
+// std::cout << "Olá mundo!" << std::endl;
+// return 0;
+// }
+
+// (a) Compile e execute o programa. Para compilar use o comando: g++ -Wall -o ola ola.cpp
+
+#include <iostream>
+
+int main(){
+    std::cout << "ola mundo" << std::endl;
+    return 0;
+}
