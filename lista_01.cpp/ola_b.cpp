0a48d39ec02aad59df68de9255b338a0a67559bb
Sara_Xavier <saraaxavierrr@gmail.com>
2024-03-05 22:13:26 -0300

    Questão 01-(B)

---
+++ lista_01.cpp/ola_b.cpp
@@ -0,0 +1,21 @@
+// A partir do programa ola.cpp a seguir
+
+// Listing 1: ola.cpp
+
+// #include <iostream>
+// int main(){
+// std::cout << "Olá mundo!" << std::endl;
+// return 0;
+// }
+
+// (b) Modifique o programa para mostrar uma segunda linha com a mensagem: Meu nome é NOME.
+// onde NOME é o seu nome. Compile e execute novamente o programa.
+
+
+#include <iostream>
+
+int main(){
+    std::cout << "Ola mundo" << std::endl;
+    std::cout << "Meu nome e Sara Xavier" << std::endl;
+    return 0;
+}
