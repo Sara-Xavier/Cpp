3f159ac36b1d80831dc074896a0115a97504f90f
Sara_Xavier <saraaxavierrr@gmail.com>
2024-03-05 22:16:13 -0300

    Questao 01-(C)

---
+++ lista_01.cpp/ola_c.cpp
@@ -0,0 +1,22 @@
+// A partir do programa ola.cpp a seguir
+
+// Listing 1: ola.cpp
+
+// #include <iostream>
+// int main(){
+// std::cout << "Olá mundo!" << std::endl;
+// return 0;
+// }
+
+// (c) Modifique novamente acrescentando mais uma linha com a mensagem: Estou aprendendo C++.
+
+
+
+#include <iostream>
+
+int main(){
+    std::cout << "Ola mundo" << std::endl;
+    std::cout << "Meu nome e Sara Xavier" << std::endl;
+    std::cout << "Estou aprendendo Cpp" << std::endl;
+    return 0;
+}
