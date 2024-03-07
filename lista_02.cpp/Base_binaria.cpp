32d139c33c135cf0d27fbf8e7fa6abd0d6389724
Sara Xavier Pires Lucena da Silva <20211014040055@ifrn.local>
2024-03-07 10:00:22 -0300

    Exercicio resolvido - 09

---
+++ lista_02.cpp/Base_binaria.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int conta_binario(int x) {
+    int qtd = 0;
+    while ( x > 0) {
+        qtd = qtd + 1;
+        x = x / 2;
+    }
+    return qtd;
+}
+
+int main(){
+    int n;
+    cin >> n;
+    int qtd_binario = conta_binario(n);
+    return 0;
+
+}
