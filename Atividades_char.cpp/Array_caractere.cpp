f89ccd9d68ac8cc7762bc0623c3d311cf02b16aa
Sara-Xavier <saraaxavierrr@gmail.com>
2024-03-14 08:51:55 -0300

    Programa que string e troca espaços por -.

---
+++ Atividades_char.cpp/Array_caractere.cpp
@@ -0,0 +1,20 @@
+//uma string pode ser vista também, como um array de caracteres
+
+#include <iostream>
+using namespace std;
+
+int main (){
+    string s;
+    getline(cin, s);
+    cout << s << endl;
+    unsigned int tamanho = s.length();
+    for (unsigned int i = 0; i < tamanho; ++i){
+        if (s[i] == ' '){
+            s[i] = '-';
+        }
+    }
+    cout << s << endl;
+    return 0;
+}
+
+// programa que recebe uma string e substitue espaços por "-"
